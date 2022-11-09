0a611853c125fd41aedf72766e6d2321f2e0549c
Noel Owolabi <emmanuelowolabiadebayo@gmail.com>
2022-11-09 14:06:17 +0100

    Props Imlementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+const User = (props) => {
+  return (
+    <div className="user">
+      <h3> Name: {props.name}</h3>
+      <h3> email: {props.email}</h3>
+      <h3> Age : {props.age}</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Props and Component Implimentation</h2>
+      <User name ="Noel Owolabi" email ="emmanuelowolabiadebayo@gmail.com" age = {20}/>
+      
     </div>
   );
 }
